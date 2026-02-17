bd7eea9d29fb2fdd64b949a342e3d6519a4b20bf
MRN14 <marinelecam14@gmail.com>
2026-02-17 15:32:10 +0100

    creating routes

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const authRouter = express.Router();
+
+// login
+authRouter.post('/login', (req, res) => {
+    res.send('login')
+})
+
+// register
+authRouter.post('/register', (req, res) => {
+    res.send('register')
+})
+
+// logout
+authRouter.get('/logout', (req, res) => {
+    res.send('logout')
+})
+
+export default authRouter;
