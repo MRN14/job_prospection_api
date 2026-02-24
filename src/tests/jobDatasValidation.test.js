ee55d5b32fa8ce4b8df4a426764eadfafd1068b1
MRN14 <marinelecam14@gmail.com>
2026-02-24 15:39:30 +0100

    writing tests to do

---
+++ src/tests/jobDatasValidation.test.js
@@ -0,0 +1,41 @@
+// TESTS JOB DATAS VALIDATION
+
+import { verifyJobDatas } from "../middlewares/jobDatasValidation";
+
+beforeAll(() => { })
+
+// TEST ROUTES
+// Routes update and create job
+
+// GLOBAL TESTS
+// Test adding other fields
+// Test changing fields order
+// Test with no fields at all
+// Test with only the name
+
+// TEST STATUS
+// Test all correct status
+// Test adding spaces arround status
+// Test incorrect status
+
+// TEST OPINION
+// Test all valid integers
+// Test float
+// Test integer below 0
+// Test integer above 5
+
+// TEST SOURCE
+// Test http & https
+// Test multiple extension
+// Test without extension
+// Test without www
+
+// TEST DATE
+// Test valid date
+// Test partial date
+// Test different format date (like YYYY:MM:DD)
+
+// TEST CONTACT
+// Test valid contact
+// Test email without @
+// Test invalid numbers
